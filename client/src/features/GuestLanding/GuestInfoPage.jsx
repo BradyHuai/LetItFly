fd658bcb94adae2959710b6f1d4f5a4827e3755c
michael <skyjoy.liu@mail.utoronto.ca>
2020-12-07 22:08:03 -0800

    Working carousel

--- client/src/features/GuestLanding/GuestInfoPage.jsx
+++ client/src/features/GuestLanding/GuestInfoPage.jsx
@@ -1,48 +1,39 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
-import paperCraneComposeImage from "../../images/paper-crane-compose.jpg";
-import paperCraneSpaceImage from "../../images/paper-crane-space.jpg";
+import red from "../../images/red.png";
+import blue from "../../images/blue.png";
+import tiga from "../../images/tiga.png";
 
 export default function GuestInfoPage() {
   return (
-    <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
-          alt="Third slide"
-        />
+    <div style={{ width: 1000, height: 200 }}>
+      <Carousel>
+        <Carousel.Item>
+          <img className="d-block w-100" src={tiga} alt="First slide" />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={red} alt="Third slide" />
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
-          alt="Third slide"
-        />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={blue} alt="Third slide" />
 
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
   );
 }
