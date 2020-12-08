5f62da54ff4ff586afe322d72535000596e8a177
michael <skyjoy.liu@mail.utoronto.ca>
2020-12-08 15:36:16 -0800

    Add styling

--- client/src/features/GuestLanding/GuestTutorial.jsx
+++ client/src/features/GuestLanding/GuestTutorial.jsx
@@ -13,89 +13,80 @@ import pic5 from "../../images/compose-screen.png";
 import pic6 from "../../images/helper.png";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        width: "100vw",
-        height: "100vh",
-        backgroundColor: "#1a2038",
-    },
-    carousel: {
-        padding: 20,
-        backgroundColor: "#ffffff",
-        borderRadius: 12,
-        width: "80vw",
-        height: "80vh",
-    },
-    button: {
-        margin: theme.spacing(1),
-        float: "right",
-    }
+  root: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100vw",
+    height: "100vh",
+    backgroundColor: "CornflowerBlue",
+  },
+  carousel: {
+    backgroundColor: "#ffffff",
+    borderRadius: 12,
+    width: 1156,
+    height: 680,
+    padding: 20,
+    position: "absolute",
+    display: "block",
+  },
+  button: {
+    margin: theme.spacing(1),
+    marginTop: "90vh",
+  },
 }));
 
 export default function GuestTutorial() {
-    const classes = useStyles();
-    const history = useHistory();
+  const classes = useStyles();
+  const history = useHistory();
 
-    const handleSubmit = (e) => {
-        history.push("/my");
-    }
+  const handleSubmit = (e) => {
+    history.push("/my");
+  };
 
-    return(
-        <CssBaseline>
-        <Paper className={classes.root}>
+  return (
+    <CssBaseline>
+      <Paper className={classes.root}>
         <Carousel className={classes.carousel}>
-                    <Carousel.Item>
-                    <img className="d-block w-100" src={pic1} alt="First slide" />
-                    <Carousel.Caption>
-                        <h3>This is the home page you will see.</h3>
-                    </Carousel.Caption>
-                    </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={pic1} alt="First slide" />
+          </Carousel.Item>
 
-                    <Carousel.Item>
-                    <img className="d-block w-100" src={pic2} alt="Third slide" />
-                    </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={pic2} alt="Third slide" />
+          </Carousel.Item>
 
-                    <Carousel.Item>
-                    <img className="d-block w-100" src={pic3} alt="Third slide" />
-                    </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={pic3} alt="Third slide" />
+          </Carousel.Item>
 
-                    <Carousel.Item>
-                    <img className="d-block w-100" src={pic4} alt="Third slide" />
-                    </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={pic4} alt="Third slide" />
+          </Carousel.Item>
 
-                    <Carousel.Item>
-                    <img className="d-block w-100" src={pic5} alt="Third slide" />
-                    </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={pic5} alt="Third slide" />
+          </Carousel.Item>
 
-                    <Carousel.Item>
-                    <img className="d-block w-100" src={pic6} alt="Third slide" />
-                    {/* <Carousel.Caption>
+          <Carousel.Item>
+            <img className="d-block w-100" src={pic6} alt="Third slide" />
+            {/* <Carousel.Caption>
                         <h3>Helper Button</h3>
                     </Carousel.Caption> */}
-                    </Carousel.Item>
-                </Carousel>
-       
-            <div>
-                <Button
-                    variant="contained"
-                    color="primary"
-                    className={classes.button}
-                    onClick={handleSubmit}
-                    >
-                    FINISH
-                </Button>
-                <Button
-                    color="secondary"
-                    className={classes.button}
-                    onClick={handleSubmit}
-                    >
-                    SKIP
-                </Button>
-            </div>
-        </Paper>
-        </CssBaseline>
-        
-    );
+          </Carousel.Item>
+        </Carousel>
+
+        <div>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            onClick={handleSubmit}
+          >
+            FINISH
+          </Button>
+        </div>
+      </Paper>
+    </CssBaseline>
+  );
 }
